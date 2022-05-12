033bbcfa7ae4806fec205ef7f92da760db519e53
Simon Chau <simon.chau1121@gmail.com>
2022-05-12 11:05:17 -0700

    Add resources to app and update gitignore

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_restful import Api
 from db import db
 
-from resources.warehouse import Warehouse
+from resources.warehouse import Warehouse, WarehouseList
+from resources.item import Item
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
@@ -13,7 +14,9 @@ api = Api(app)
 def create_tables():
     db.create_all()
 
-api.add_resource(Warehouse, "api/warehouse/<string:name>")
+api.add_resource(Warehouse, "/api/warehouse/<string:name>")
+api.add_resource(WarehouseList, "/api/warehouses")
+api.add_resource(Item, "/api/item/<string:name>&<int:warehouse_id>")
 
 if __name__ == "__main__":
     db.init_app(app)
