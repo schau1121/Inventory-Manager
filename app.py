05aa6d8b9a100afc9faed6e68ffdb1e2d7bb9a24
Simon Chau <simon.chau1121@gmail.com>
2022-05-11 20:08:21 -0700

    Add project structure and set up flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Api
+from db import db
+
+from resources.warehouse import Warehouse
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+api.add_resource(Warehouse, "/warehouse/<string:name>")
+
+if __name__ == "__main__":
+    db.init_app(app)
+    app.run(port=5000, debug=True)
