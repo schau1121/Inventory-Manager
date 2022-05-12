96f25370a9fcee629f6210bec7139cd7bc5014db
Simon Chau <simon.chau1121@gmail.com>
2022-05-11 20:22:56 -0700

    Add warehouse model

---
+++ models/warehouse.py
@@ -0,0 +1,34 @@
+from db import db
+class WarehouseModel(db.Model):
+    __tablename__ = "warehouses"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    location = db.Column(db.String(100))
+    
+    items = db.relationship("ItemModel", lazy="dynamic")
+    
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+    
+    def json(self):
+        return {'name': self.name,
+                "location": self.location,
+                "items": [item.json() for item in self.items.all()]}
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_by_location(cls, location):
+        return cls.query.filter_by(location=location).first()
