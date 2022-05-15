bc0104d73dfc6fcd80867b47670f42c16769de8f
Simon Chau <simon.chau1121@gmail.com>
2022-05-15 14:32:42 -0700

    Create templates for frontend and configure jinja

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 from db import db
 
 from resources.warehouse import Warehouse, WarehouseList
-from resources.item import Item
+from resources.item import AllItemsList, Item, ItemList
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
@@ -14,9 +14,23 @@ api = Api(app)
 def create_tables():
     db.create_all()
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/items")
+def items():
+    return render_template("items.html")
+
+@app.route("/warehouses")
+def warehouses():
+    return render_template("warehouses.html")
+
 api.add_resource(Warehouse, "/api/warehouse/<string:name>")
 api.add_resource(WarehouseList, "/api/warehouses")
 api.add_resource(Item, "/api/item/<string:name>&<int:warehouse_id>")
+api.add_resource(ItemList, "/api/item/<string:name>")
+api.add_resource(AllItemsList, "/api/items")
 
 if __name__ == "__main__":
     db.init_app(app)
