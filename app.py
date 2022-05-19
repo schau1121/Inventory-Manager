3d15dd4fd218ef144faca44cb7cc494c479e5e57
Simon Chau <simon.chau1121@gmail.com>
2022-05-19 17:26:43 +0000

    Fix host in app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ api.add_resource(AllItemsList, "/api/items")
 
 if __name__ == "__main__":
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
